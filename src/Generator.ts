ab15aa0a98124c6ee6eacbcd4909bb85d41a799b
Matthew Hatcher <matthewh@halfpetal.com>
2019-07-31 07:41:20 -0400

    Adding the reqs check.

--- src/Generator.ts
+++ src/Generator.ts
@@ -1,9 +1,13 @@
 import * as inquirer from "inquirer"
 import * as path from "path"
 import * as fs from "fs-extra"
+import { execSync } from "child_process" 
+import * as semver from "semver"
+import * as cmdExists from "command-exists"
 
 export class Generator {
   private fullPath: string = null;
+  private minPyVersion: string = ">= 3.7.2"
   constructor(private projectName: string, private projectPath: string) {
     this.fullPath = path.resolve(this.projectPath != undefined? this.projectPath : "", this.projectName)
     console.log("Generating new project. You'll be asked a couple questions throughout this process.")
@@ -46,6 +50,28 @@ export class Generator {
         return fs.mkdirp(this.fullPath)
       })
       .then(() => {
+        return inquirer.prompt([
+          {
+            type: "confirm",
+            name: "check_prereq",
+            message: "Would you like to check for proper Python3 version and Git before generating the project?",
+            default: false
+          }
+        ])
+      })
+      .then((answers: any) => {
+        if (answers["check_prereq"]) {
+          let pythonValid: boolean = semver.satisfies(execSync("python -c \"import platform; print(platform.python_version())\"").toString(), this.minPyVersion)
+          let gitValid: boolean = cmdExists.sync("git")
+
+          if (!pythonValid || !gitValid) {
+            console.error("Requirements are not met. Exiting.")
+            process.exit(1)
+          } else {
+            console.log("All requirements are met. Moving onto project generation.")
+          }
+        }
+
         this.generate()
       })
   }
